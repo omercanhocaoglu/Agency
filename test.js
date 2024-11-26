d694fc845fb2672ba396ea42eb0b625deb9e5468
Ömer Can <omercanhocaoglu@gmail.com>
2024-11-26 20:22:45 +0300

    Mongoose crud işlemleri tamamlanmıştır.

---
+++ test.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// connect DB
+mongoose.connect('mongodb://localhost/agency-test-db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//create Schema
+const PhotoSchema = new Schema({
+    title: String,
+    description: String
+});
+
+const Photo = mongoose.model('Photo', PhotoSchema);
+
+// create a photo
+// Photo.create({
+//     title: "Photo Title 2",
+//     description: "Photo Description 2 lorem ipsum"
+// });
+
+// read photo
+Photo.find({}, (err, data) => {
+    console.log(data);
+});
